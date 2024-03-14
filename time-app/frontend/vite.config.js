d663c8ec981089f0b0ace8acb674af2fa82e463c
yevdokymenko90 <yevdokymenko90@gmail.com>
2024-03-14 01:27:43 +0100

    added api hot reload

--- time-app/frontend/vite.config.js
+++ time-app/frontend/vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -11,4 +10,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: { 
+    watch: {
+      usePolling: true,
+    }
+  }
 })
